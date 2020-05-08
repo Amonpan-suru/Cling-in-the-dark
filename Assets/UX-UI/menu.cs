b71053881365372df8146af628fb5aa3d490484c
Amonpan-suru <amonpan.suru@mail.kmutt.ac.th>
2020-05-09 06:09:45 +0700

    แทรกหน้าเมนู

---
+++ Assets/UX-UI/menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonlink : MonoBehaviour
+{
+    
+private void playgame()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    private void Quitgame() => Application.Quit();
+
+}
