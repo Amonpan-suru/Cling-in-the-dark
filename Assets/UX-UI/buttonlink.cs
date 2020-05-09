12cb677c881cc4ba6de4e0ccfc72c658e8ba657d
Amonpan-suru <amonpan.suru@mail.kmutt.ac.th>
2020-05-09 13:29:12 +0700

    first page

--- Assets/UX-UI/buttonlink.cs
+++ Assets/UX-UI/buttonlink.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class buttonlink : MonoBehaviour
 {
-    
-private void playgame()
+public void playgame()
     {
         SceneManager.LoadScene("Scene1");
     }
 
-    private void Quitgame() => Application.Quit();
+public void Quitgame(){
+    Application.Quit();
+}
 
 }
