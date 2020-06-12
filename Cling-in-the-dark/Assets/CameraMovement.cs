26541cba5ff1bed3ff65b00cab8a258e0c0ee768
mix_yspt <61342894+mixyspt@users.noreply.github.com>
2020-06-13 00:01:14 +0700

    13/6/2020

---
+++ Cling-in-the-dark/Assets/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Cling-in-the-dark/Assets/LightDecrease.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDecrease : MonoBehaviour
+{
+    public Light Light1;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if( collision.gameObject.tag == "DarkHand" )
+        {
+            Light1.intensity -= 0.5f;
+        }
+    }
+}
