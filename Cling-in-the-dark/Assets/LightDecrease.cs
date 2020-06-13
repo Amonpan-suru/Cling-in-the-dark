6ac4ad580530607bac38f353099a75c17d8845bd
mix_yspt <61342894+mixyspt@users.noreply.github.com>
2020-06-13 16:43:15 +0700

    13/6/2020_2

--- Cling-in-the-dark/Assets/LightDecrease.cs
+++ Cling-in-the-dark/Assets/LightDecrease.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class LightDecrease : MonoBehaviour
 {
     public Light Light1;
+    public Component CameraMovement;
+    public Component PlayerMovement;
+
+
+    private void Start()
+    {
+        CameraMovement1 = gameObject.GetComponent<CameraMovement>();
+        PlayerMovement1 = gameObject.GetComponent<PlayerMovement>();
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -12,5 +21,11 @@ public class LightDecrease : MonoBehaviour
         {
             Light1.intensity -= 0.5f;
         }
-    }
+
+        if( Light1.intensity == 0.0f )
+        {
+            CameraMovement.enabled = !CameraMovement.enabled;
+            PlayerMovement.enabled = !PlayerMovement.enabled;           
+        }
+    }  
 }
