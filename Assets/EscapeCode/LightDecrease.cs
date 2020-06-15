0206744e97593874af7d0aa6ed8452a31cf4e418
mix_yspt <61342894+mixyspt@users.noreply.github.com>
2020-06-15 12:03:07 +0700

    15/6/2020_2

---
+++ Assets/EscapeCode/LightDecrease.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDecrease : MonoBehaviour
+{
+    public Light Light1;
+
+    private static bool Alive = true;
+
+    public static bool getAlive { get { return Alive; } }
+
+    private void OnTriggerEnter( Collider collision )
+    {
+        if ( collision.gameObject.tag == "DarkHand" )
+        {
+            Light1.intensity -= 0.5f;
+        }
+        if (collision.gameObject.tag == "CrystalChase")
+        {
+            Light1.intensity -= 10.0f;
+        }
+        if ( Light1.intensity == 0.0f )
+        {
+            Alive = false;
+        }
+        
+    }
+}
---
+++ Assets/EscapeCode/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    private void Update()
+    {
+        if (LightDecrease.getAlive)
+        {
+            PlayerMovement1();
+        }
+    }
+    void PlayerMovement1()
+    {   
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = speed + 2.0f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = speed - 2.0f;
+        }
+    }  
+}
