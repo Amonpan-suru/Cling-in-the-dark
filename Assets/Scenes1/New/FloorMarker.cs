77b8c0b7564c44633b717a1f049208362be7a4ab
film110TH <61343092+film110TH@users.noreply.github.com>
2020-06-17 22:32:28 +0700

    17/06/2020=floormarker

---
+++ Assets/Scenes1/New/FloorMarker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloorMarker : MonoBehaviour
+{
+    public Text floormarker;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "floor1")
+        {
+            floormarker.text = "floor1";
+        }
+        if (other.name == "floor2")
+        {
+            floormarker.text = "floor2";
+        }
+        if (other.name == "floor3")
+        {
+            floormarker.text = "floor3";
+        }
+        if (other.name == "floor4")
+        {
+            floormarker.text = "upperfloor";
+        }
+        if (other.name == "F3-1")
+        {
+            floormarker.text = "f3-1";
+        }
+    }
+}
