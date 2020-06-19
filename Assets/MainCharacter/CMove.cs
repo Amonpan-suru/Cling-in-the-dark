782c1fa267d3982b05c2a670f44998203aa924e4
film110TH <61343092+film110TH@users.noreply.github.com>
2020-06-19 22:17:23 +0700

    19/06/2020 = fianl

--- Assets/MainCharacter/CMove.cs
+++ Assets/MainCharacter/CMove.cs
@@ -5,40 +5,37 @@ using UnityEngine;
 public class CMove : MonoBehaviour
 {
     [SerializeField] float speed;
+
+    private bool walk;
     void Update()
     {
        
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += new Vector3(-speed*Time.deltaTime, 0f, 0f);
-
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += new Vector3(speed*Time.deltaTime, 0f, 0f);
-
         }
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += new Vector3(0f, 0f, speed * Time.deltaTime);
-
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += new Vector3(0f, 0f, -speed * Time.deltaTime);
-
         }
+        
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                speed = speed + 2f;
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                speed = speed - 2f;
+            }
+        
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            speed = speed + 2f;
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            speed = speed - 2f;
-        }
-
-
-       
     }
 }
