deec30bce6858ffa3f9f82375d5b580cd3dee17d
Amonpan-suru <amonpan.suru@mail.kmutt.ac.th>
2020-06-19 15:03:19 +0700

    3
    แก้ไดอะหลอกให้มีภาพ

---
+++ Assets/UX-UI/script/colliWarp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class colliWarp : MonoBehaviour
+{
+    public string scenename;
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+}
---
+++ Assets/UX-UI/script/triggerDi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerDi : MonoBehaviour
+{
+    public GameObject player;
+    public Dialogue dialogue;
+    public GameObject me;
+    public GameObject other;
+    public GameObject other2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        me.gameObject.SetActive(true);
+        other.gameObject.SetActive(false);
+        other2.gameObject.SetActive(false);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        Destroy(gameObject);
+    }
+}
