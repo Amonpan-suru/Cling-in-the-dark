e4338ff955e7da27ff6e13cceedce43720b3cbab
Amonpan-suru <amonpan.suru@mail.kmutt.ac.th>
2020-06-21 14:21:26 +0700

    98%

---
+++ Assets/UX-UI/script/CloseOpen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseOpen : MonoBehaviour
+{
+    public GameObject Close;
+    public GameObject Open;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Close.gameObject.SetActive(false);
+        Open.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
